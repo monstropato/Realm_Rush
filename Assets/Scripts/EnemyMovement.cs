d6d4ac365e3b3bc1c13452861ee9c9ec269366fb
Felipe Camêlo <felipecamelobrx@gmail.com>
2021-04-27 12:17:29 -0300

    now the enemies move smoothly

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     //CONFIG PARAMS
-    [SerializeField] float waitTime = 1f;
+    [SerializeField][Range(0f,5f)] float speed =1f;
 
     //CACHED CLASSES REFERENCES
     Enemy enemy;
@@ -18,10 +18,21 @@ public class EnemyMovement : MonoBehaviour
 
     private IEnumerator FollowPatch()
     {
+        //transform.position = enemy.patch[0].transform.position;
         foreach(Waypoint waypoint in enemy.patch)
         {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPos = transform.position;
+            Vector3 endPos = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPos);
+
+            while (travelPercent < 1)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPos, endPos, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
